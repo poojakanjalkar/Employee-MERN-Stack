351451c9cff1132f16de61c200f64ff85c73e6a0
Pooja Kanjalkar <poojakanjalkar554@gmail.com>
2024-05-10 22:00:13 +0530

    added CORS to handle cors error

--- Backend/app.js
+++ Backend/app.js
@@ -1,9 +1,11 @@
 const express = require("express");
-
+const cors = require("cors");
 const bodyParser = require("body-parser");
 const employeeRoutes = require("./routes/employee.route");
 const { connectToDB } = require("./routes/utils/dbUtils");
 const app = express();
+// Enable CORS for all origins
+app.use(cors());
 app.use(express.json());
 app.use("/employees", employeeRoutes);
 
